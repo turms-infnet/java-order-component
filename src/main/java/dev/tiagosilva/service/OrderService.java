c6c79dcecc25a2c9448a92c4c3d916db51277193
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2025-05-13 18:56:51 -0300

    First commit

---
+++ src/main/java/dev/tiagosilva/service/OrderService.java
@@ -0,0 +1,15 @@
+package dev.tiagosilva.service;
+
+public class OrderService {
+    public void createOrder() {
+        System.out.println("Creating an order...");
+    }
+
+    public void updateOrder() {
+        System.out.println("Updating an order...");
+    }
+
+    public void deleteOrder() {
+        System.out.println("Deleting an order...");
+    }
+}
